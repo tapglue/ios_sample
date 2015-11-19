a215c383ab7b177a7404718d043bac5de608e098
Onur Akpolat <onur.akpolat@gmail.com>
2015-11-19 22:33:45 +0100

    Add edit function to user

--- TapglueSample/ProfileVC.swift
+++ TapglueSample/ProfileVC.swift
@@ -38,6 +38,45 @@ class ProfileVC: UIViewController {
         self.navigationController?.performSegueWithIdentifier("loginSegue", sender: nil)
     }
     
+    @IBAction func editButtonPressed(sender: AnyObject) {
+        let user = TGUser.currentUser()
+        
+        let alertController = UIAlertController(title: "Change Username", message: "Enter new username:", preferredStyle: .Alert)
+        alertController.view.frame = CGRectMake(0.0, 0.0, 320.0, 400.0)
+        
+        let confirmAction = UIAlertAction(title: "Confirm", style: .Default) { (_) in
+            let newUsername = alertController.textFields?.first?.text
+            if (newUsername != nil && newUsername?.characters.count > 2) {
+                user.username = newUsername
+                user.saveWithCompletionBlock { (success : Bool, error : NSError!) -> Void in
+                    if success {
+                        // Implementation success.
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            self.userNameLabel.text = newUsername
+                        })
+                        print("Username changed: \(newUsername) ")
+                    } else {
+                        // Implement error handling.
+                        print("Server error")
+                    }
+                }
+            } else {
+                print("Username wrong")
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (_) in }
+        
+        alertController.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = user.username
+        }
+        
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
     @IBAction func thumbEventButton(sender: AnyObject) {
         Tapglue.createEventWithType("Likes your status", onObjectWithId: "ThumbFilled")
         
