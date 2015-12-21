4d2f19b437d77e29928a006c2f3708e4b0b5dd3c
Tristan Celebi <oezguercelebi@gmail.com>
2015-12-21 18:13:34 +0100

    Commented out

--- TapglueSample/FeedVC.swift
+++ TapglueSample/FeedVC.swift
@@ -1,78 +1,78 @@
+////
+////  ActivityVC.swift
+////  TapglueSample
+////
+////  Created by Onur Akpolat on 23.10.15.
+////  Copyright © 2015 Tapglue. All rights reserved.
+////
 //
-//  ActivityVC.swift
-//  TapglueSample
+//import UIKit
+//import Tapglue
 //
-//  Created by Onur Akpolat on 23.10.15.
-//  Copyright © 2015 Tapglue. All rights reserved.
+//class FeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+//    
+//    @IBOutlet weak var activityTableView: UITableView!
+//    
+//    // Tapglue events array
+//    var events: [TGEvent] = []
+//    
+//    var refreshControl: UIRefreshControl!
 //
-
-import UIKit
-import Tapglue
-
-class FeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
-    @IBOutlet weak var activityTableView: UITableView!
-    
-    // Tapglue events array
-    var events: [TGEvent] = []
-    
-    var refreshControl: UIRefreshControl!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        activityTableView.delegate = self
-        activityTableView.dataSource = self
-        
-        self.refreshControl = UIRefreshControl()
-        self.refreshControl.addTarget(self, action: "loadFriendsActivityFeed", forControlEvents: UIControlEvents.ValueChanged)
-        self.activityTableView.addSubview(refreshControl)
-        self.activityTableView.sendSubviewToBack(refreshControl)
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        self.refreshControl?.beginRefreshing()
-        self.loadFriendsActivityFeed()
-    }
-    
-    func refresh(sender:AnyObject)
-    {
-        self.refreshControl?.beginRefreshing()
-        self.loadFriendsActivityFeed()
-    }
-    
-    func loadFriendsActivityFeed() {
-        Tapglue.retrieveNewsFeedForCurrentUserWithCompletionBlock { (posts : [AnyObject]!, feed : [AnyObject]!, error : NSError!) -> Void in
-            if error != nil {
-                print("Error happened\n")
-                print(error)
-            }
-            else {
-                dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                    self.events = feed as! [TGEvent]
-                    self.activityTableView.reloadData()
-                })
-                self.refreshControl.endRefreshing()
-            }
-        }
-    }
-    
-    /*
-    * TableView Methods
-    */
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.events.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! EventTableViewCell
-        let event = self.events[indexPath.row]
-        cell.configureCellWithEvent(event)
-        
-        return cell
-    }
-}
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        activityTableView.delegate = self
+//        activityTableView.dataSource = self
+//        
+//        self.refreshControl = UIRefreshControl()
+//        self.refreshControl.addTarget(self, action: "loadFriendsActivityFeed", forControlEvents: UIControlEvents.ValueChanged)
+//        self.activityTableView.addSubview(refreshControl)
+//        self.activityTableView.sendSubviewToBack(refreshControl)
+//    }
+//    
+//    override func viewWillAppear(animated: Bool) {
+//        self.refreshControl?.beginRefreshing()
+//        self.loadFriendsActivityFeed()
+//    }
+//    
+//    func refresh(sender:AnyObject)
+//    {
+//        self.refreshControl?.beginRefreshing()
+//        self.loadFriendsActivityFeed()
+//    }
+//    
+//    func loadFriendsActivityFeed() {
+//        Tapglue.retrieveNewsFeedForCurrentUserWithCompletionBlock { (posts : [AnyObject]!, feed : [AnyObject]!, error : NSError!) -> Void in
+//            if error != nil {
+//                print("Error happened\n")
+//                print(error)
+//            }
+//            else {
+//                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                    self.events = feed as! [TGEvent]
+//                    self.activityTableView.reloadData()
+//                })
+//                self.refreshControl.endRefreshing()
+//            }
+//        }
+//    }
+//    
+//    /*
+//    * TableView Methods
+//    */
+//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        return 1
+//    }
+//    
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return self.events.count
+//    }
+//    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! EventTableViewCell
+//        let event = self.events[indexPath.row]
+//        cell.configureCellWithEvent(event)
+//        
+//        return cell
+//    }
+//}
--- TapglueSample/FriendsVC.swift
+++ TapglueSample/FriendsVC.swift
@@ -1,88 +1,88 @@
+////
+////  FriendsVC.swift
+////  TapglueSample
+////
+////  Created by Onur Akpolat on 05/11/15.
+////  Copyright © 2015 Tapglue. All rights reserved.
+////
 //
-//  FriendsVC.swift
-//  TapglueSample
+//import UIKit
+//import Tapglue
 //
-//  Created by Onur Akpolat on 05/11/15.
-//  Copyright © 2015 Tapglue. All rights reserved.
-//
-
-import UIKit
-import Tapglue
-
-class FriendsVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
-    @IBOutlet weak var friendsTableView: UITableView!
-    
-    // Tapglue users array
-    var users: [TGUser] = []
-    
-    var refreshControl: UIRefreshControl!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        friendsTableView.delegate = self
-        friendsTableView.dataSource = self
-        
-        self.refreshControl = UIRefreshControl()
-        self.refreshControl.addTarget(self, action: "loadFriends", forControlEvents: UIControlEvents.ValueChanged)
-        self.friendsTableView.addSubview(refreshControl)
-        self.friendsTableView.sendSubviewToBack(refreshControl)
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        
-        super.viewWillAppear(animated)
-        self.loadFriends()
-    }
-    
-    func loadFriends() {
-        self.refreshControl?.beginRefreshing()
-        Tapglue.retrieveFriendsForCurrentUserWithCompletionBlock { (users : [AnyObject]!, error : NSError!) -> Void in
-            if users != nil && error == nil {
-                dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                    self.users = users as! [TGUser]
-                    self.friendsTableView.reloadData()
-                })
-            } else if error != nil{
-                print("Error happened\n")
-                print(error)
-            }
-            self.refreshControl?.endRefreshing()
-        }
-    }
-    
-    /*
-    * TableView Methods
-    */
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.users.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UserTableViewCell
-        let user = self.users[indexPath.row]
-        cell.configureCellWithUser(user)
-        
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == UITableViewCellEditingStyle.Delete {
-            Tapglue.unfriendUser(users[indexPath.row]) { (success : Bool, error : NSError!) -> Void in
-                if success {
-                    print("User unfriend successful")
-                } else if error != nil{
-                    print("Error happened\n")
-                    print(error)
-                }
-            }
-            users.removeAtIndex(indexPath.row)
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
-        }
-    }
-}
+//class FriendsVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+//    
+//    @IBOutlet weak var friendsTableView: UITableView!
+//    
+//    // Tapglue users array
+//    var users: [TGUser] = []
+//    
+//    var refreshControl: UIRefreshControl!
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        friendsTableView.delegate = self
+//        friendsTableView.dataSource = self
+//        
+//        self.refreshControl = UIRefreshControl()
+//        self.refreshControl.addTarget(self, action: "loadFriends", forControlEvents: UIControlEvents.ValueChanged)
+//        self.friendsTableView.addSubview(refreshControl)
+//        self.friendsTableView.sendSubviewToBack(refreshControl)
+//    }
+//    
+//    override func viewWillAppear(animated: Bool) {
+//        
+//        super.viewWillAppear(animated)
+//        self.loadFriends()
+//    }
+//    
+//    func loadFriends() {
+//        self.refreshControl?.beginRefreshing()
+//        Tapglue.retrieveFriendsForCurrentUserWithCompletionBlock { (users : [AnyObject]!, error : NSError!) -> Void in
+//            if users != nil && error == nil {
+//                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                    self.users = users as! [TGUser]
+//                    self.friendsTableView.reloadData()
+//                })
+//            } else if error != nil{
+//                print("Error happened\n")
+//                print(error)
+//            }
+//            self.refreshControl?.endRefreshing()
+//        }
+//    }
+//    
+//    /*
+//    * TableView Methods
+//    */
+//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        return 1
+//    }
+//    
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return self.users.count
+//    }
+//    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UserTableViewCell
+//        let user = self.users[indexPath.row]
+//        cell.configureCellWithUser(user)
+//        
+//        return cell
+//    }
+//    
+//    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+//        if editingStyle == UITableViewCellEditingStyle.Delete {
+//            Tapglue.unfriendUser(users[indexPath.row]) { (success : Bool, error : NSError!) -> Void in
+//                if success {
+//                    print("User unfriend successful")
+//                } else if error != nil{
+//                    print("Error happened\n")
+//                    print(error)
+//                }
+//            }
+//            users.removeAtIndex(indexPath.row)
+//            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+//        }
+//    }
+//}
