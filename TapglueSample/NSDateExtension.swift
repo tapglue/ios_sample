113309ea314f22b73e38f129578da357dddb18b5
Tristan Celebi <oezguercelebi@gmail.com>
2015-12-29 02:34:03 +0100

    Added new date to string format

--- TapglueSample/NSDateExtension.swift
+++ TapglueSample/NSDateExtension.swift
@@ -28,4 +28,61 @@ extension NSDate {
         formatter.dateFormat = format
         return formatter.stringFromDate(self)
     }
+    
+    func timeFormatInElapsedTimeToString() -> String {
+        // Get the time of event creation
+        let elapsedTime = NSDate().timeIntervalSinceDate(self)
+        // Save elapsedTime as Int
+        let duration = Int(elapsedTime)
+        
+        // Handle Int duration
+        let seconds = duration % 60
+        let minutes = (duration / 60) % 60
+        let hours = duration / 3600
+        let days = duration / 86400
+        let weeks = duration / 604800
+        let strWeeks = days > 9 ? String(weeks) : "" + String(weeks)
+        let strDays = days > 9 ? String(days) : "" + String(days)
+        let strHours = hours > 9 ? String(hours) : "" + String(hours)
+        let strMinutes = minutes > 9 ? String(minutes) : "" + String(minutes)
+        let strSeconds = seconds > 9 ? String(seconds) : "" + String(seconds)
+        
+        
+        if weeks > 1 {
+            return "\(strWeeks) weeks"
+        }
+        else if weeks == 1 {
+            return "\(strWeeks) week"
+        }
+        else if days > 1 {
+            return "\(strDays) days"
+        }
+        else if days == 1
+        {
+            return "yesterday"
+        }
+        else if hours > 1 {
+            return "\(strHours) hrs"
+        }
+        else if hours == 1
+        {
+            return "\(strHours) hrs"
+        }
+        else if minutes > 1
+        {
+            return "\(strMinutes) min"
+        }
+        else if minutes == 1
+        {
+            return "\(strMinutes) min"
+        }
+        else if seconds > 1
+        {
+            return "\(strSeconds) sec"
+        }
+        else
+        {
+            return "\(strSeconds) sec"
+        }
+    }
 }
