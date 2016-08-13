5fd6d5ff0af35041f67b28300f18e31ff50685c2
Tristan Celebi <oezguercelebi@gmail.com>
2016-08-13 17:06:01 +0200

    Refactoring to Enums and Switches

--- TapglueSample/FindUsersTableViewCell.swift
+++ TapglueSample/FindUsersTableViewCell.swift
@@ -19,6 +19,9 @@ class FindUsersTableViewCell: UITableViewCell {
     
     @IBOutlet weak var connectLeftButton: UIButton!
     @IBOutlet weak var connectRightButton: UIButton!
+    
+    
+    let inviteTag = 10
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -33,30 +36,26 @@ class FindUsersTableViewCell: UITableViewCell {
         
         let meta = user.metadata as AnyObject
         self.userAboutLabel.text = String(meta.valueForKey("about")!)
-        
         self.userNameLabel.text = self.cellUser.username
         
-        // UserImage
-        var userImage = TGImage()
-        userImage = cellUser.images.valueForKey("profilePic") as! TGImage
-        self.userImageView.kf_setImageWithURL(NSURL(string: userImage.url)!)
+        showUserImage(cellUser)
         
         if self.connectRightButton != nil {
             if cellUser.isFriend {
-                friendUserCustomizeButton()
+                customizeBtn(.Friend)
                 self.connectRightButton.selected = true
             } else {
-                unfriendUserCustomizeButton()
+                customizeBtn(.Unfriend)
                 self.connectRightButton.selected = false
             }
         }
         
         if self.connectLeftButton != nil {
             if cellUser.isFollowed {
-                followingUserCustomizeButton()
+                customizeBtn(.Follow)
                 self.connectLeftButton.selected = true
             } else {
-                unfollowingUserCustomizeButton()
+                customizeBtn(.Unfollow)
                 self.connectLeftButton.selected = false
             }
         }
@@ -70,19 +69,14 @@ class FindUsersTableViewCell: UITableViewCell {
         
         self.userAboutLabel.text = String(meta.valueForKey("about")!)
         self.userNameLabel.text = self.cellUser.username
-        
-        
-        // UserImage
-        var userImage = TGImage()
-        userImage = cellUser.images.valueForKey("profilePic") as! TGImage
-        self.userImageView.kf_setImageWithURL(NSURL(string: userImage.url)!)
+        showUserImage(cellUser)
         
         if self.connectRightButton != nil {
-            acceptFriendShipCustomizeButton()
+            customizeBtn(.Accept)
         }
         
         if self.connectLeftButton != nil {
-            declineFriendShipCustomizeButton()
+            customizeBtn(.Decline)
         }
     }
     
@@ -90,19 +84,33 @@ class FindUsersTableViewCell: UITableViewCell {
     func configureCellWithUserFromContactsThatUsesApp(contact: [String:String], user: TGUser){
         cellUser = user
         
-        self.userAboutLabel.text = ""
+        let meta = cellUser.metadata as AnyObject
+        self.userAboutLabel.text = String(meta.valueForKey("about")!)
         self.userNameLabel.text = contact["givenName"]
         
-        self.connectRightButton.hidden = false
-        self.connectLeftButton.hidden = true
+        showUserImage(cellUser)
         
         if self.connectRightButton != nil {
+            self.connectRightButton.hidden = false
+            
             if user.isFriend {
-                self.friendUserCustomizeButton()
+                customizeBtn(.Unfriend)
                 self.connectRightButton.selected = true
             } else {
-                self.unfriendUserCustomizeButton()
+                customizeBtn(.Friend)
                 self.connectRightButton.selected = false
+            }
+        }
+        if self.connectLeftButton != nil {
+            self.connectLeftButton.hidden = false
+            
+            if user.isFollower {
+//                self.followingUserCustomizeButton()
+                customizeBtn(.Unfollow)
+                self.connectLeftButton.selected = true
+            } else {
+                customizeBtn(.Follow)
+                self.connectLeftButton.selected = false
             }
         }
     }
@@ -110,105 +118,103 @@ class FindUsersTableViewCell: UITableViewCell {
     // Configure Cell with Contacts that need to be invited
     func configureCellWithUserFromContacts(contact: [String:String]){
         // TODO: Add invite process
-        self.userAboutLabel.text = ""
+        self.userAboutLabel.text = contact["email"]
         self.userNameLabel.text = contact["givenName"]
         
-        inviteCustomizeButton()
+        customizeBtn(.Invite)
     }
     
     @IBAction func connectRightPressed(sender: UIButton) {
-        if sender.selected {
-            Tapglue.unfriendUser(cellUser, withCompletionBlock: { (success: Bool,error: NSError!) -> Void in
-                if success {
-                    print("User unfriend successful")
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        sender.selected = false
-                        self.unfriendUserCustomizeButton()
-                    })
-                } else if error != nil{
-                    print("\nError unfriendUser: \(error)")
-                }
-            })
-        } else {
-            Tapglue.friendUser(cellUser, withState: TGConnectionState.Pending, withCompletionBlock: { (success : Bool, error : NSError!) -> Void in
-                if success {
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        sender.selected = true
-                        self.pendingUserCustomizeButton()
-                    })
-                } else if error != nil{
-                    print("\nError friendUser: \(error)")
-                }
-            })
-        }
+        connectRightBtnState(sender)
     }
     
     @IBAction func connectLeftPressed(sender: UIButton) {
-        if sender.selected {
-            Tapglue.unfollowUser(cellUser, withCompletionBlock: { (success: Bool, error: NSError!) -> Void in
-                if success {
-                    print("User unfollow successful")
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        sender.selected = false
-                        self.unfollowingUserCustomizeButton()
-                    })
-                } else if error != nil{
-                    print("\nError unfollowUser: \(error)")
-                }
-            })
-        } else {
-            Tapglue.followUser(cellUser, withCompletionBlock: { (success: Bool, error: NSError!) -> Void in
-                if success {
-                    print("User follow successful: \(success)")
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        sender.selected = true
-                        self.followingUserCustomizeButton()
-                    })
-                } else if error != nil{
-                    print("\nError followUser: \(error)")
-                }
-            })
+        connectLeftBtnState(sender)
+    }
+    
+    
+//    func followingUserCustomizeButton(){
+//        self.connectLeftButton.setTitle("Following", forState: .Selected)
+//        self.connectLeftButton.backgroundColor = UIColor(red:0.016, green:0.859, blue:0.675, alpha:1)
+//        self.connectLeftButton.tag = BtnState.Unfollow.rawValue
+//    }
+//    func unfollowingUserCustomizeButton(){
+//        self.connectLeftButton.setTitle("Follow", forState: .Normal)
+//        self.connectLeftButton.backgroundColor = UIColor.lightGrayColor()
+//        self.connectLeftButton.tag = BtnState.Follow.rawValue
+//    }
+//    
+//    func friendUserCustomizeButton(){
+//        self.connectRightButton.setTitle("Friend", forState: .Selected)
+//        self.connectRightButton.backgroundColor = UIColor(red:0.016, green:0.859, blue:0.675, alpha:1)
+//        self.connectRightButton.tag = BtnState.Unfriend.rawValue
+//    }
+//    func unfriendUserCustomizeButton(){
+//        self.connectRightButton.setTitle("Add Friend", forState: .Normal)
+//        self.connectRightButton.backgroundColor = UIColor.lightGrayColor()
+//        self.connectRightButton.tag = BtnState.Friend.rawValue
+//    }
+//    
+//    func pendingUserCustomizeButton(){
+//        self.connectRightButton.setTitle("Pending", forState: .Selected)
+//        self.connectRightButton.backgroundColor = UIColor.orangeColor()
+//        self.connectRightButton.tag = BtnState.Pending.rawValue
+//    }
+//    
+//    func acceptFriendShipCustomizeButton(){
+//        self.connectRightButton.setTitle("Accept", forState: .Normal)
+//        self.connectRightButton.backgroundColor = UIColor.orangeColor()
+//        self.connectRightButton.tag = BtnState.Accept.rawValue
+//    }
+//    func declineFriendShipCustomizeButton(){
+//        self.connectLeftButton.setTitle("Decline", forState: .Normal)
+//        self.connectLeftButton.backgroundColor = UIColor.redColor()
+//        self.connectLeftButton.tag = BtnState.Decline.rawValue
+//    }
+//    
+//    func inviteCustomizeButton(){
+//        self.connectLeftButton.setTitle("Invite", forState: .Normal)
+//        self.connectLeftButton.backgroundColor = UIColor.lightGrayColor()
+//        self.connectLeftButton.tag = BtnState.Invite.rawValue
+//        self.connectRightButton.hidden = true
+//    }
+    
+    func customizeBtn(state: BtnState) {
+        switch state {
+        case .Invite:
+            self.connectLeftButton.setTitle("Invite", forState: .Normal)
+            self.connectLeftButton.backgroundColor = UIColor.lightGrayColor()
+            self.connectLeftButton.tag = BtnState.Invite.rawValue
+            self.connectRightButton.hidden = true
+        case .Follow:
+            self.connectLeftButton.setTitle("Following", forState: .Selected)
+            self.connectLeftButton.backgroundColor = UIColor(red:0.016, green:0.859, blue:0.675, alpha:1)
+            self.connectLeftButton.tag = BtnState.Follow.rawValue
+        case .Unfollow:
+            self.connectLeftButton.setTitle("Follow", forState: .Normal)
+            self.connectLeftButton.backgroundColor = UIColor.lightGrayColor()
+            self.connectLeftButton.tag = BtnState.Unfollow.rawValue
+        case .Friend:
+            self.connectRightButton.setTitle("Add Friend", forState: .Normal)
+            self.connectRightButton.backgroundColor = UIColor.lightGrayColor()
+            self.connectRightButton.tag = BtnState.Friend.rawValue
+        case .Unfriend:
+            self.connectRightButton.setTitle("Friend", forState: .Selected)
+            self.connectRightButton.backgroundColor = UIColor(red:0.016, green:0.859, blue:0.675, alpha:1)
+            self.connectRightButton.tag = BtnState.Unfriend.rawValue
+        case .Pending:
+            self.connectRightButton.setTitle("Pending", forState: .Selected)
+            self.connectRightButton.backgroundColor = UIColor.orangeColor()
+            self.connectRightButton.tag = BtnState.Pending.rawValue
+        case .Accept:
+            self.connectRightButton.setTitle("Accept", forState: .Normal)
+            self.connectRightButton.backgroundColor = UIColor.orangeColor()
+            self.connectRightButton.tag = BtnState.Accept.rawValue
+        case .Decline:
+            self.connectLeftButton.setTitle("Decline", forState: .Normal)
+            self.connectLeftButton.backgroundColor = UIColor.redColor()
+            self.connectLeftButton.tag = BtnState.Decline.rawValue
         }
-    }
-    
-    
-    func followingUserCustomizeButton(){
-        self.connectLeftButton.setTitle("Following", forState: .Selected)
-        self.connectLeftButton.backgroundColor = UIColor(red:0.016, green:0.859, blue:0.675, alpha:1)
-    }
-    func unfollowingUserCustomizeButton(){
-        self.connectLeftButton.setTitle("Follow", forState: .Normal)
-        self.connectLeftButton.backgroundColor = UIColor.lightGrayColor()
-    }
-    
-    func friendUserCustomizeButton(){
-        self.connectRightButton.setTitle("Friend", forState: .Selected)
-        self.connectRightButton.backgroundColor = UIColor(red:0.016, green:0.859, blue:0.675, alpha:1)
-    }
-    func unfriendUserCustomizeButton(){
-        self.connectRightButton.setTitle("Add Friend", forState: .Normal)
-        self.connectRightButton.backgroundColor = UIColor.lightGrayColor()
-    }
-    
-    func pendingUserCustomizeButton(){
-        self.connectRightButton.setTitle("Pending", forState: .Selected)
-        self.connectRightButton.backgroundColor = UIColor.orangeColor()
-    }
-    
-    func acceptFriendShipCustomizeButton(){
-        self.connectRightButton.setTitle("Accept", forState: .Normal)
-        self.connectRightButton.backgroundColor = UIColor.orangeColor()
-    }
-    func declineFriendShipCustomizeButton(){
-        self.connectLeftButton.setTitle("Decline", forState: .Normal)
-        self.connectLeftButton.backgroundColor = UIColor.redColor()
-    }
-    
-    func inviteCustomizeButton(){
-        self.connectLeftButton.setTitle("Invite", forState: .Normal)
-        self.connectLeftButton.backgroundColor = UIColor.lightGrayColor()
-        
-        self.connectRightButton.hidden = true
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
@@ -217,4 +223,112 @@ class FindUsersTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    
+    func showUserImage(cellUser: TGUser) {
+        // UserImage
+        var userImage = TGImage()
+        userImage = cellUser.images.valueForKey("profilePic") as! TGImage
+        self.userImageView.kf_setImageWithURL(NSURL(string: userImage.url)!)
+    }
+    
+    func connectLeftBtnState(sender: UIButton){
+        switch sender.tag {
+        case BtnState.Invite.rawValue:
+            print("Invite")
+        case BtnState.Follow.rawValue:
+            print("Follow")
+            Tapglue.followUser(cellUser, withCompletionBlock: { (success: Bool, error: NSError!) -> Void in
+                if success {
+                    print("User follow successful: \(success)")
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        sender.selected = true
+                        self.customizeBtn(.Follow)
+                    })
+                } else if error != nil{
+                    print("\nError followUser: \(error)")
+                }
+            })
+        case BtnState.Unfollow.rawValue:
+            print("Unfollow")
+            Tapglue.unfollowUser(cellUser, withCompletionBlock: { (success: Bool, error: NSError!) -> Void in
+                if success {
+                    print("User unfollow successful")
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        sender.selected = false
+                        self.customizeBtn(.Unfollow)
+                    })
+                } else if error != nil{
+                    print("\nError unfollowUser: \(error)")
+                }
+            })
+        case BtnState.Decline.rawValue:
+            print("Decline")
+            Tapglue.friendUser(cellUser, withState: TGConnectionState.Rejected, withCompletionBlock: { (success : Bool, error : NSError!) -> Void in
+                if success {
+                    print(success)
+                } else if error != nil{
+                    print("\nError friendUser: \(error)")
+                }
+            })
+        default:
+            print("default left")
+        }
+    }
+    
+    func connectRightBtnState(sender: UIButton){
+        switch sender.tag {
+        case BtnState.Friend.rawValue:
+            print("Friend")
+            Tapglue.friendUser(cellUser, withState: TGConnectionState.Pending, withCompletionBlock: { (success : Bool, error : NSError!) -> Void in
+                if success {
+                    print("Do friendship: \(success)")
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        sender.selected = true
+                        self.customizeBtn(.Pending)
+                    })
+                } else if error != nil{
+                    print("\nError friendUser: \(error)")
+                }
+            })
+        case BtnState.Unfriend.rawValue:
+            print("Unfriend")
+            Tapglue.unfriendUser(cellUser, withCompletionBlock: { (success: Bool,error: NSError!) -> Void in
+                if success {
+                    print("User unfriend successful")
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        sender.selected = false
+                        self.customizeBtn(.Unfriend)
+                    })
+                } else if error != nil{
+                    print("\nError unfriendUser: \(error)")
+                }
+            })
+        case BtnState.Pending.rawValue:
+            print("Still Pending")
+            
+        case BtnState.Accept.rawValue:
+            print("Accept")
+            Tapglue.friendUser(cellUser, withState: TGConnectionState.Confirmed, withCompletionBlock: { (success : Bool, error : NSError!) -> Void in
+                if success {
+                    print("Confirmed friendship: \(success)")
+                } else if error != nil{
+                    print("\nError friendUser: \(error)")
+                }
+            })
+            
+        default:
+            print("default right")
+        }
+    }
+    
+    enum BtnState: Int {
+        case Invite = 10
+        case Follow = 20
+        case Unfollow = 21
+        case Friend = 30
+        case Unfriend = 31
+        case Pending = 40
+        case Accept = 41
+        case Decline = 42
+    }
 }
