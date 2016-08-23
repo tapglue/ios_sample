84ed687d865b5a1fe91ddf708f74c0867a097a92
Tristan Celebi <oezguercelebi@gmail.com>
2016-08-23 20:08:39 +0200

    Fixed pass usr

--- TapglueSample/FilteredTagsTableViewCell.swift
+++ TapglueSample/FilteredTagsTableViewCell.swift
@@ -95,6 +95,7 @@ class FilteredTagsTableViewCell: UITableViewCell {
         
         // Pass the relevant data to the new sub-ViewController
         pdVC.post = cellPost
+        pdVC.usr = cellPost.user
         pdVC.commentButtonPressedSwitch = true
         
         rootViewController.pushViewController(pdVC, animated: true)
