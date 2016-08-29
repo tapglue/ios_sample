e3c85f5278869ef904afd90e2c34a61ac8958404
Tristan Celebi <oezguercelebi@gmail.com>
2016-08-29 18:45:14 +0200

    Added notification handle

--- TapglueSample/AppDelegate.swift
+++ TapglueSample/AppDelegate.swift
@@ -36,7 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         config.baseUrl = url
         config.appToken = appToken
         // If you like to see logs: set true
-        config.log = true
+        config.log = false
         rxTapglue = RxTapglue(configuration: config)
         
         sims = TapglueSims(withConfiguration: config, environment: .Sandbox)
@@ -49,6 +49,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let configuration = AWSServiceConfiguration(region: region, credentialsProvider: credentialsProvider)
         AWSServiceManager.defaultServiceManager().defaultServiceConfiguration = configuration
         
+        // Handle push notification if app is inactive
+        if let remoteNotification = launchOptions?[UIApplicationLaunchOptionsRemoteNotificationKey] as! [NSObject : AnyObject]? {
+            //do stuff with notification
+            self.application(application, didReceiveRemoteNotification: remoteNotification)
+        }
         
         return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
     }
@@ -69,24 +74,71 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         print("Failed to register:", error)
     }
     func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
-        print("UserInfo: \(userInfo)")
-        print(userInfo)
         
-        if let aps = userInfo["aps"] as? NSDictionary {
-            if let alert = aps["alert"] as? NSDictionary {
-                if let message = alert["message"] as? NSString {
-                    //Do stuff
-                    print("Message: \(message)")
-                }
-            } else if let alert = aps["alert"] as? NSString {
-                //Do stuff
-                print("Alert: \(alert)")
+        if let urn = NSURL(string: userInfo["urn"] as! String) {
+            switch urn.pathComponents![1] {
+            case "posts":
+                retrievePostWithIDAndUserWithID(urn.pathComponents![urn.pathComponents!.indexOf("posts")!+1])
+            case "users":
+                retrieveUserWithID(urn.pathComponents![urn.pathComponents!.indexOf("users")!+1])
+            default: ""
             }
         }
-        
+    
     }
     
-    // TapglueError handel
+    //Mark: - Tapglue
+    func retrievePostWithIDAndUserWithID(postID: String) {
+        rxTapglue.retrievePost(postID).subscribe({ (event) in
+            switch event {
+            case .Next(let post):
+                
+                let rootViewController = self.window?.rootViewController as! UINavigationController
+                let storyboard = UIStoryboard(name: "PostDetail", bundle: nil)
+                let pdVC = storyboard.instantiateViewControllerWithIdentifier("PostDetailViewController") as!
+                PostDetailVC
+                
+                pdVC.post = post
+                
+                self.rxTapglue.retrieveUser(post.userId!).subscribe { (event) in
+                    switch event {
+                    case .Next(let usr):
+                        pdVC.usr = usr
+                        
+                    case .Error(let error):
+                        self.printOutErrorMessageAndCode(error as? TapglueError)
+                    case .Completed:
+                        rootViewController.pushViewController(pdVC, animated: false)
+                    }
+                    }.addDisposableTo(self.disposeBag)
+                
+            case .Error(let error):
+                self.printOutErrorMessageAndCode(error as? TapglueError)
+            case .Completed:
+                break
+            }
+        }).addDisposableTo(disposeBag)
+    }
+    
+    func retrieveUserWithID(userID: String) {
+        let rootViewController = self.window?.rootViewController as! UINavigationController
+        let storyboard = UIStoryboard(name: "UserProfile", bundle: nil)
+        let upVC = storyboard.instantiateViewControllerWithIdentifier("UserProfileViewController") as!
+        UserProfileVC
+        
+        self.rxTapglue.retrieveUser(userID).subscribe { (event) in
+            switch event {
+            case .Next(let usr):
+                upVC.userProfile = usr
+                
+            case .Error(let error):
+                self.printOutErrorMessageAndCode(error as? TapglueError)
+            case .Completed:
+                rootViewController.pushViewController(upVC, animated: false)
+            }
+        }.addDisposableTo(self.disposeBag)
+    }
+    
     func printOutErrorMessageAndCode(err: TapglueError?) {
         print(err?.code)
         print(err?.message)
